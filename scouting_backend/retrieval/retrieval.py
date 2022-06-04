f55894cafe48d47e7317cd34f936c7b867aa0a51
eagle9518 <j007wo2@gmail.com>
2022-06-04 11:48:53 -0700

    Connected QR Result to SQL Alchemy

--- scouting_backend/retrieval/retrieval.py
+++ scouting_backend/retrieval/retrieval.py
@@ -1,4 +1,6 @@
-from flask import render_template, Blueprint
+from flask import render_template, Blueprint, request
+
+from .models import db, matchEntry
 
 retrieval_bp = Blueprint(
     'retrieval_bp', __name__,
@@ -11,50 +13,26 @@ retrieval_bp = Blueprint(
 def qrScanner():
     return render_template('QRScanner.html')
 
-# @retrieval_bp.route('/requests', methods=['POST', 'GET'])
-# def tasks():
-#     global switch, camera
-#     if request.method == 'POST':
-#         if request.form.get('stop') == 'Stop/Start':
-#             if switch == 1:
-#                 switch = 0
-#                 camera.release()
-#                 cv2.destroyAllWindows()
-#             else:
-#                 camera = cv2.VideoCapture(0)
-#                 switch = 1
-#                 while switch == 1:
-#                     ret, frame = camera.read()
-#                     cv2.imshow('Image', frame)
-#                     code = cv2.waitKey(10)
-#
-#                     gray_img = cv2.cvtColor(frame, 0)
-#                     barcode = decode(gray_img)
-#
-#                     for obj in barcode:
-#                         barcodeData = str(obj.data.decode("utf-8")).split(',')
-#                         print(barcodeData)
-#                         existing_match = matchEntry.query.filter(
-#                             matchEntry.team == barcodeData[0] or matchEntry.matchNumber ==
-#                             barcodeData[1]).first()
-#                         if not existing_match:
-#                             match = matchEntry(
-#                                 team=int(barcodeData[0]),
-#                                 matchNumber=int(barcodeData[1]),
-#                                 autoCrossing=int(barcodeData[2]),
-#                                 autoUpper=int(barcodeData[3]),
-#                                 autoBottom=int(barcodeData[4]),
-#                                 teleUpper=int(barcodeData[5]),
-#                                 teleBottom=int(barcodeData[6]),
-#                                 level=int(barcodeData[7]),
-#                                 driverPerf=int(barcodeData[8]),
-#                                 defensePerf=int(barcodeData[9]),
-#                                 name=str(barcodeData[10]),
-#                                 comment=str(barcodeData[11]),
-#                             )
-#                             db.session.add(match)
-#                             db.session.commit()
-#
-#     elif request.method == 'GET':
-#         return render_template('QRScanner.html')
-#     return render_template('QRScanner.html')
+
+@retrieval_bp.route('/detectScan', methods=['POST'])
+def test():
+    QRData = request.form['data'].split(',')
+    existing_match = matchEntry.query.filter(matchEntry.team == QRData[0] or matchEntry.matchNumber == QRData[1]).first()
+    if not existing_match:
+        match = matchEntry(
+            team=int(QRData[0]),
+            matchNumber=int(QRData[1]),
+            autoCrossing=int(QRData[2]),
+            autoUpper=int(QRData[3]),
+            autoBottom=int(QRData[4]),
+            teleUpper=int(QRData[5]),
+            teleBottom=int(QRData[6]),
+            level=int(QRData[7]),
+            driverPerf=int(QRData[8]),
+            defensePerf=int(QRData[9]),
+            name=str(QRData[10]),
+            comment=str(QRData[11]),
+        )
+        db.session.add(match)
+        db.session.commit()
+    return render_template('QRScanner.html')
--- scouting_backend/retrieval/static/qrCodeScanner.js
+++ scouting_backend/retrieval/static/qrCodeScanner.js
@@ -11,48 +11,64 @@ const btnScanQR = document.getElementById("btn-scan-qr");
 let scanning = false;
 
 qrcode.callback = res => {
-  if (res) {
-    outputData.innerText = res;
-    scanning = false;
+    if (res) {
+        if (outputData.innerText !== res) {
+            if (res.split(',').length !== 12) {
+                outputData.innerText = "ERROR Not A Valid QR Code"
+            } else {
+                outputData.innerText = res;
+                $.post("/retrieval/detectScan", {
+                    data: res
+                });
+            }
+        }
+        qrResult.hidden = false;
 
-    video.srcObject.getTracks().forEach(track => {
-      track.stop();
-    });
-
-    qrResult.hidden = false;
-    canvasElement.hidden = true;
-    btnScanQR.hidden = false;
-  }
+        tick()
+        scan()
+    }
 };
 
 btnScanQR.onclick = () => {
-  navigator.mediaDevices
-    .getUserMedia({ video: { facingMode: "environment" } })
-    .then(function(stream) {
-      scanning = true;
-      qrResult.hidden = true;
-      btnScanQR.hidden = true;
-      canvasElement.hidden = false;
-      video.setAttribute("playsinline", true);
-      video.srcObject = stream;
-      video.play();
-      tick();
-      scan();
-    });
+    if (scanning) {
+        scanning = false;
+
+        video.srcObject.getTracks().forEach(track => {
+            track.stop();
+        });
+
+        qrResult.hidden = true;
+        canvasElement.hidden = true;
+        btnScanQR.hidden = false;
+    } else {
+        navigator.mediaDevices
+            .getUserMedia({video: {facingMode: "environment"}})
+            .then(function (stream) {
+                scanning = true;
+                qrResult.hidden = true;
+                btnScanQR.hidden = false;
+                canvasElement.hidden = false;
+                video.setAttribute("playsinline", true);
+                video.srcObject = stream;
+                video.play();
+                tick();
+                scan();
+            });
+    }
 };
 
 function tick() {
-  canvasElement.height = video.videoHeight;
-  canvasElement.width = video.videoWidth;
-  canvas.drawImage(video, 0, 0, canvasElement.width, canvasElement.height);
+    canvasElement.height = video.videoHeight;
+    canvasElement.width = video.videoWidth;
+    canvas.drawImage(video, 0, 0, canvasElement.width, canvasElement.height);
 
-  scanning && requestAnimationFrame(tick);
+    scanning && requestAnimationFrame(tick);
 }
 
 function scan() {
-  try {
-    qrcode.decode();
-  } catch (e) {
-    setTimeout(scan, 300);
-  }
+    try {
+        qrcode.decode();
+    } catch (e) {
+        setTimeout(scan, 400);
+    }
 }
