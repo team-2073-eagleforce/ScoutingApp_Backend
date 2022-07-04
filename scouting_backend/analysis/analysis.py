ad476bdb5fe17025ca121fa9ae2eec8be4c31ba9
eagle9518 <j007wo2@gmail.com>
2022-07-03 17:46:54 -0700

    Finished Rankings

--- scouting_backend/analysis/analysis.py
+++ scouting_backend/analysis/analysis.py
@@ -3,12 +3,16 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import scoped_session, sessionmaker
 
 import os
-from scouting_backend.tba import get_match_schedule
+from scouting_backend.tba import get_match_schedule, get_match_team
 
 import google.oauth2.credentials
 import google_auth_oauthlib.flow
 from googleapiclient import discovery
 from scouting_backend import sheet
+
+import json
+
+import numpy as np
 
 os.environ['OAUTHLIB_INSECURE_TRANSPORT'] = '1'
 os.environ['OAUTHLIB_RELAX_TOKEN_SCOPE'] = '1'
@@ -164,16 +168,6 @@ def analysis_dashboard():
     return render_template("dashboard/dashboard.html")
 
 
-@analysis_bp.route("/api/get_match_schedule/<string:event_key>/<string:match_num>")
-def api_get_match_schedule(event_key, match_num):
-    # {"red": ["frc1", "frc2", "frc3"], "blue": ["frc4", "frc5", "frc6"]}
-    teams_in_match = get_match_schedule(event_key, int(match_num))
-    positions = ['red1', 'red2', 'red3', 'blue1', 'blue2', 'blue3']
-    averages_for_teams_in_match = dict(zip(positions, calculate_averages(tuple(pos.split('frc')[1] for pos in teams_in_match['red'] + teams_in_match['blue']))))
-
-    return jsonify(averages_for_teams_in_match)
-
-
 @analysis_bp.route("/rankings", methods=['GET', 'POST'])
 def rankings_list():
     return render_template("rankings.html")
@@ -182,27 +176,75 @@ def rankings_list():
 @analysis_bp.route("/sorter", methods=['GET', 'POST'])
 def sorter():
     sort_by = request.form['button_selected']
+    all_teams = tuple(int(team['team_number']) for team in get_match_team('2022cafr'))
+    team_data_for_selected = dict(zip(all_teams, fetch_sql_for_rankings(all_teams, sort_by)))
+    return jsonify(team_data_for_selected)
 
 
-def calculate_averages(teams):
+def fetch_sql_for_rankings(all_teams, sort_by):
+    a = {'by_auto': '"team", "autoCrossing", "autoUpper", "autoBottom"', 'by_teleop': '"team", "teleUpper", "teleBottom"',
+         'by_climb': '"team", "level"', 'by_total': '"team", "autoCrossing", "autoUpper", "autoBottom", "teleUpper", "teleBottom", "level"'}
+
+    team_with_selected_data_values = db.execute("""SELECT {sort_by} FROM scouting WHERE team IN {teams}""".format(sort_by=a[sort_by], teams=all_teams)).fetchall()
+    dic_team_with_average = dict(zip(all_teams, ([0] * 2 for team in range(len(all_teams)))))
+
+    for match in team_with_selected_data_values:
+        print(match)
+        score_points = [[0, 2, 4, 2], [0, 2, 1], [0, 2, 4, 2, 2, 1, 0]]
+        team_score_for_datatype = dic_team_with_average[match[0]]
+
+        if sort_by == "by_auto":
+            team_score_for_datatype[0] += sum(np.multiply(match, score_points[0]))
+        elif sort_by == "by_teleop":
+            team_score_for_datatype[0] += sum(np.multiply(match, score_points[1]))
+        elif sort_by == "by_climb":
+            team_score_for_datatype[0] += CONST_CLIMB_POINTS[match[1]]
+        elif sort_by == "by_total":
+            team_score_for_datatype[0] += sum(np.multiply(match, score_points[2])) + CONST_CLIMB_POINTS[match[6]]
+        team_score_for_datatype[1] += 1
+
+    average_score_for_field = []
+    for score_sum in dic_team_with_average.values():
+        if score_sum[1] == 0:
+            score_sum[1] = 1
+        average_score_for_field.append(round(score_sum[0] / score_sum[1], 2))
+
+    return average_score_for_field
+
+
+@analysis_bp.route("/api/get_match_schedule/<string:event_key>/<string:match_num>")
+def api_get_match_schedule(event_key, match_num):
+    # {"red": ["frc1", "frc2", "frc3"], "blue": ["frc4", "frc5", "frc6"]}
+    teams_in_match = get_match_schedule(event_key, int(match_num))
+    positions = ['red1', 'red2', 'red3', 'blue1', 'blue2', 'blue3']
+    averages_for_teams_in_match = dict(zip(positions, fetch_sql_for_dashboard(tuple(pos.split('frc')[1] for pos in teams_in_match['red'] + teams_in_match['blue']))))
+
+    return jsonify(averages_for_teams_in_match)
+
+
+def fetch_sql_for_dashboard(teams):
     matches_for_team = db.execute("""SELECT * FROM scouting WHERE team IN {teams}""".format(teams=teams)).fetchall()
     dic_matches_for_team = dict(zip(teams, ([int(teams[team])] + [0] * 7 for team in range(len(teams)))))
 
     for match in matches_for_team:
-        team_sum = dic_matches_for_team[str(match[1])]
+        team = dic_matches_for_team[str(match[1])]
 
         calculated_points_per_section = calculate_points(match)
 
-        team_sum[1] += match[4]
-        team_sum[2] += match[5]
-        team_sum[3] += match[6]
-        team_sum[4] += match[7]
-        team_sum[5] += match[8]
-        team_sum[6] += calculated_points_per_section[3]
-        team_sum[7] += 1
+        team[1] += match[4]
+        team[2] += match[5]
+        team[3] += match[6]
+        team[4] += match[7]
+        team[5] += match[8]
+        team[6] += calculated_points_per_section[3]
+        team[7] += 1
 
+    return calculate_average(dic_matches_for_team)
+
+
+def calculate_average(sum_of_data):
     all_averages = []
-    for averages in dic_matches_for_team.values():
+    for averages in sum_of_data.values():
         average_div = [averages[0]]
         for data_val in averages[1:-1]:
             if averages[-1] == 0:
