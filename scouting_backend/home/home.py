7915671e4bf4083a454dc0906df02fd31cef6d71
eagle9518 <j007wo2@gmail.com>
2022-07-07 11:04:16 -0700

    removed previous dashBoard in home blueprint

--- scouting_backend/home/home.py
+++ scouting_backend/home/home.py
@@ -11,24 +11,4 @@ home_bp = Blueprint(
 @home_bp.route("/login", methods=['GET', 'POST'])
 def login():
     error = None
-
-    if request.method == 'POST':
-        if request.form['username'] != 'admin' or request.form['password'] != 'admin':
-            error = 'Invalid username or password. Please try again!'
-        else:
-            session['username'] = request.form['username']
-            return redirect(url_for('home_bp.dashBoard'))
-
     return render_template('login.html', error=error)
-
-
-@home_bp.route('/logout')
-def logout():
-    session.pop('username', None)
-    session.clear()
-    return redirect(url_for('home_bp.login'))
-
-
-@home_bp.route("/dashBoard", methods=["GET", "POST"])
-def dashBoard():
-    return render_template("dashboard/dashboard.html")
