10f6b7a1ddf1b3a405a9b3df61b7f5d47dac2651
Boyuan Liu <boyuanliu6@yahoo.com>
2022-07-17 01:02:30 +0000

    fixed search query

--- scouting_backend/analysis/analysis.py
+++ scouting_backend/analysis/analysis.py
@@ -47,7 +47,7 @@ def team_navigation():
 @login_required
 @analysis_bp.route("/team/<int:team>", methods=["GET"])
 def view_team_data(team):
-    comp_code = request.args.get("comp")
+    comp_code = request.args.get("code")
     matches = db.execute("SELECT * FROM scouting WHERE team=:team AND comp_code=:comp ORDER BY matchnumber ASC", {"team": team, "comp": comp_code})
     matches_with_calculated_scores = []
 
